a83036dc19c5e92350cf82962650e3652a87e363
progfay <shun1856shun@gmail.com>
2020-08-22 22:54:02 +0900

    feat: impl wrap.Before

---
+++ wrap/before.go
@@ -0,0 +1,37 @@
+package wrap
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func Before(dst, src interface{}, beforeFn func()) error {
+	vDst := reflect.ValueOf(dst)
+	vSrc := reflect.ValueOf(src)
+
+	if vDst.Kind() != reflect.Ptr {
+		return fmt.Errorf("First argument of WiretapWithArgs must be pointer of function")
+	}
+
+	vDst = vDst.Elem()
+	if vDst.Kind() != reflect.Func {
+		return fmt.Errorf("First argument of WiretapWithArgs must be pointer of function")
+	}
+
+	if vSrc.Kind() != reflect.Func {
+		return fmt.Errorf("Second argument of WiretapWithArgs must be function")
+	}
+
+	if vDst.Type() != vSrc.Type() {
+		return fmt.Errorf("Value of first argument must be same type function as second argument")
+	}
+
+	f := func(values []reflect.Value) []reflect.Value {
+		beforeFn()
+		return vSrc.Call(values)
+	}
+
+	vDst.Set(reflect.MakeFunc(vDst.Type(), f))
+
+	return nil
+}
